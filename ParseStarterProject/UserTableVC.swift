344d208583d9e800b1d0c3d9437f62e517330c94
Daniel J Janiak <danieljjaniak@icloud.com>
2016-10-20 21:00:31 -0700

    feat: Enable users to follow other users
    A new class for the follower-following relationship was created within
    the Parse dashboard.  Notably, the default behavior of Parse makes
    it so that a user cannot follow his or herself.  The following status is
    established simply by clicking the table view cell that is displaying a
    given user's name.

--- ParseStarterProject/UserTableVC.swift
+++ ParseStarterProject/UserTableVC.swift
@@ -11,15 +11,20 @@ import Parse
 
 class UserTableVC: UITableViewController {
     
-    // MARK: - Properties 
+    // MARK: - Properties
     
     let cellIdentifier = "UserCell"
     var userNames = [""]
+    var userIDs = [""]
     
     // MARK: - Lifecycle
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let currentUser = PFUser.current() {
+            print("\n\n**The current user is: \(currentUser)** \n\n")
+        }
         
         let query = PFUser.query()
         
@@ -42,7 +47,10 @@ class UserTableVC: UITableViewController {
                     /* Append all current users */
                     if let user = item as? PFUser {
                         
-                        self.userNames.append(user.username!)
+                        let usernameComponents = user.username!.components(separatedBy: "@")
+                        
+                        self.userNames.append(usernameComponents[0])
+                        self.userIDs.append(item.objectId!)
                         
                     }
                     
@@ -53,87 +61,105 @@ class UserTableVC: UITableViewController {
             self.tableView.reloadData()
             
         })
-
+        
     }
+    
+    // MARK: - Table View Delegate Methods
     
     override func viewDidAppear(_ animated: Bool) {
         
         /* Show the navigation bar */
         self.navigationController?.navigationBar.isHidden = false
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return userNames.count
     }
-
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {        
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
         
         cell.textLabel?.text = userNames[indexPath.row]
-
+        
         return cell
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let cell = tableView.cellForRow(at: indexPath)
+        
+        cell?.accessoryType = UITableViewCellAccessoryType.checkmark
+        
+        /* Set up the follower - following relationship */
+        
+        let following = PFObject(className: "Followers")
+        
+        following["follower"] = PFUser.current()?.objectId
+        following["following"] = userIDs[indexPath.row]
+        
+        following.saveInBackground()
     }
-    */
-
+    
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
     
     // MARK: - Actions
     
@@ -145,5 +171,5 @@ class UserTableVC: UITableViewController {
         
     }
     
-
+    
 }
